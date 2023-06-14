18245b33f9529b1f9bc54e21aa7e5c9480de806a
alexmzeier <alexander.zeier@outlook.de>
2023-06-14 15:36:23 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useState } from "react";
+import { BrowserRouter, Route, Link, redirect } from "react-router-dom";
+
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from 'react-speech-recognition';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const commands = [
+    {
+      command: ["Go to * page", "Go to *", "Open * page", "Open *"],
+      callback: (redirectPage: any) => setRedirectUrl(redirectPage),
+    },
+  ];
+
+  const { transcript } = useSpeechRecognition({ commands });
+  const [redirectUrl, setRedirectUrl] = useState("");
+  const pages = ["home", "blog", "new blog post", "contact"];
+  const urls = {
+    home: "/",
+    blog: "/blog",
+    "new blog post": "/blog/new",
+    contact: "/contact",
+  };
+
+
+
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return null;
+  }
+
+  let redirect = "";
+
+  async function clickButton(){
+    SpeechRecognition.startListening()
+    console.log("Click Button")
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,7 +54,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>        
+        <p id="transcript">Transcript: {transcript}</p>
+        <button onClick={() => clickButton()}>Start</button>
       </header>
     </div>
   );
